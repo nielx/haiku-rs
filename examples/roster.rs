3e2a09306744ad4d65cda4e40f32e5c12876eb3b
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2019-02-13 21:43:06 +0100

    Application Kit: expose structs on the module level

--- examples/roster.rs
+++ examples/roster.rs
@@ -1,6 +1,6 @@
 extern crate haiku;
 
-use haiku::app::roster::ROSTER;
+use haiku::app::ROSTER;
 
 const NAME_FIELD_WIDTH: usize = 34;
 
--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,8 +1,16 @@
 //
-// Copyright 2018, Niels Sascha Reedijk <niels.reedijk@gmail.com>
+// Copyright 2018-2019, Niels Sascha Reedijk <niels.reedijk@gmail.com>
 // All rights reserved. Distributed under the terms of the MIT License.
 //
 
-pub mod message;
-pub mod messenger;
-pub mod roster;
+//! The application kit contains tools to structure your application and to
+//! communicate with other applications and services
+
+
+mod message;
+mod messenger;
+mod roster;
+
+pub use self::message::Message;
+pub use self::messenger::Messenger;
+pub use self::roster::{ROSTER, AppInfo, Roster};
