66a756ba9b9ce4fc56decfa334caa608fcd640db
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2019-06-16 11:41:33 +0200

    Storage: add limits for vpaths and filenames

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -6,6 +6,16 @@
 //! Tools to manipulate the file system and the Haiku specific extentions to
 //! it
 
+use libc::{FILENAME_MAX, PATH_MAX};
+
 mod attributes;
 
 pub use self::attributes::{AttributeDescriptor, AttributeIterator, AttributeExt};
+
+// Kit constants
+pub const B_DEV_NAME_LENGTH: usize = 128;
+pub const B_FILE_NAME_LENGTH: usize = FILENAME_MAX as usize;
+pub const B_PATH_NAME_LENGTH: usize = PATH_MAX as usize;
+pub const B_ATTR_NAME_LENGTH: usize = FILENAME_MAX as usize - 1;
+pub const B_MIME_TYPE_LENGTH: usize = B_ATTR_NAME_LENGTH as usize - 15;
+pub const B_MAX_SYMLINKS: usize = 16; // This does not seem to be exposed in libc
