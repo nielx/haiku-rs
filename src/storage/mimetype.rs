3e1b9c498a5b72416eee3fb2bd76f1a9537381d4
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2020-02-13 22:57:33 +0100

    Storage Kit: document MimeType

--- src/storage/mimetype.rs
+++ src/storage/mimetype.rs
@@ -1,23 +1,28 @@
 //
-// Copyright 2019, Niels Sascha Reedijk <niels.reedijk@gmail.com>
+// Copyright 2019-2020, Niels Sascha Reedijk <niels.reedijk@gmail.com>
 // All rights reserved. Distributed under the terms of the MIT License.
 //
 
 use ::storage::B_MIME_TYPE_LENGTH;
 
+/// Represents a mime type as defined by RFC 6838
 #[derive(PartialEq)]
 pub struct MimeType {
 	type_string: String
 }
 
 impl MimeType {
+	/// Create a MimeType based on a string
+	///
+	/// The entered data is validated. A `MimeType` is only created when the
+	/// data is valid, otherwise you will receive `None`.
 	pub fn new(mime_type: &str) -> Option<Self> {
 		if mime_type.len() > B_MIME_TYPE_LENGTH {
 			return None;
 		}
-		
+
 		let mut found_slash = false;
-		
+
 		for (i, ch) in mime_type.chars().enumerate() {
 			if ch == '/' {
 				if found_slash || i == 0 || i == (mime_type.len() - 1) {
@@ -33,16 +38,20 @@ impl MimeType {
 					return None;
 			}
 		}
-		
+
 		Some(MimeType {
 			type_string: String::from(mime_type)
 		})
 	}
 
+	/// Check if the mime type only defines the super type
 	pub fn is_supertype_only(&self) -> bool {
 		!self.type_string.contains('/')
 	}
 
+	/// Get the super type of this mimetype
+	///
+	/// For example, `text/plain` will return `text`.
 	pub fn get_supertype(&self) -> MimeType {
 		if self.is_supertype_only() {
 			MimeType {
